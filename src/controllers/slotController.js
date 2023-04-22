bce963af06b8380a414c46b3b5e034e55d617f2c
Rakesh Kumar <aaryanrakeshsingh1530@gmail.com>
2023-04-22 20:44:39 +0530

    assigment_in_mysql

--- src/controllers/slotController.js
+++ src/controllers/slotController.js
@@ -1,22 +1,33 @@
-const slotModel = require("../models/slotModel")
+const con = require("../config")
+const timeSlot = (req, res) => {
+    try {
+        const data = req.body
+        const {id, Date, Start_time, End_time } = data
+        if (!id) return res.status(400).send({ status: false, message: "please provide a unique number Id like(1)" })
+        if (!Date) return res.status(400).send({ status: false, message: "Date is mandatory , like((YYYY/MM/DD)" })
+        if (!Start_time) return res.status(400).send({ status: false, message: "Start_time is mandatory , like(HH:MM)" })
+        if (!End_time) return res.status(400).send({ status: false, message: "End_time is mandatory,  like(HH:MM)" })
 
-const timeSlot=async (req,res)=>{
-    try{
-    const data=req.body
-    const {Date,Start_time,End_time}=data
-    if(!Date) return res.status(400).send({status:false, message:"Date is mandatory , like((DD/MM/YYYY)"})
-    if(!Start_time) return res.status(400).send({status:false, message:"Start_time is mandatory , like(HH:MM)"})
-    if(!End_time) return res.status(400).send({status:false, message:"End_time is mandatory,  like(HH:MM)"})
+        con.query("SELECT * FROM slot WHERE Date=? and Start_time=? and End_time=? ", [Date, Start_time, End_time],
+            function (err, rows) {
+                if (err) {
+                    console.log("err")
+                }
+                if (!rows.length) {
+                    con.query("INSERT INTO slot Set ?", data, (error, result, fields) => {
+                        if (error) return res.status(400).send({ status: false, message: "please change your id || In between time slot is booked, so please choise another time slot" })
+                        return res.status(201).send({ status: true, message: "Time_slot SaveData", data })
+                    })
+                } else {
+                    console.log("already exist")
+                    return res.status(400).send({ status: false, message: "This slot time is already Exist, please select another Timeslot" })
+                }
+            })
 
-    const checkSlot= await slotModel.findOne({Date:Date,Start_time:Start_time,End_time:End_time})
-    if(checkSlot) return res.status(400).send({status:false, message:"This slot time is already Exist, please select another Timeslot"})
-    
-    const saveSlot= await slotModel.create(data)
 
-    return res.status(201).send({status:true, message:"Time_slot Saved", saveSlot})
-    }catch(err){
-        return res.status(500).send({status:false,message:err.message})
+    } catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
     }
 }
 
-module.exports={timeSlot}
+module.exports = { timeSlot }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const route= require("./routes/route");
-
+const con=require("./config")
 const app=express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://Rak18000:Rakesh123@cluster0.xntrj.mongodb.net/Time_slot",{
-    useNewUrlParser:true
-}).then(()=>console.log("mongoDb is connected"))
-.catch(err=>console.log(err))
+
 
 app.use("/", route)
 
